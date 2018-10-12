48f7a17e79797d088424cd39bef37c65e0809ae8
a6926578 <254153639@qq.com>
2018-10-12 21:33:41 +0800

    Add Demo.java and update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello
 {
 	public static void main(String []args){
 	
-	System.out.println("www.baidu.com");
-	System.out.println("www.taobao.com")	
+	System.out.println("www.jingdong.com");
 	}
 }
