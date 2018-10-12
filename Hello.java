3e0972f76aa0d252cff90a37998a0d84d4da5d62
a6926578 <254153639@qq.com>
2018-10-12 21:25:20 +0800

    增加了两个网址输入;

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main(String []args){
 	
-		System.out.println("hello__word");
+	System.out.println("www.baidu.com");
+	System.out.println("www.taobao.com")	
 	}
 }
