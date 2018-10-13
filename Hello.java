91b25d8ee7dce731e42cbd247810a7365d5c1228
a6926578 <254153639@qq.com>
2018-10-14 00:44:08 +0800

    add a contranct;

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello
 {
 	public static void main(String []args){
-	
-	System.out.println("KT NEW B");
-	System.out.println("EDG caibi");
+		public static final IG="IG jiayou";
+		System.out.println("KT NEW B");
+		System.out.println("EDG caibi");
 
 	}
 }
